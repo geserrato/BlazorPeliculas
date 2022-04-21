98633e0ba2ce7b1a48a970fe71ac954fcafefdb7
Gerardo Estrella <gerardo.estrella@sygno.com.mx>
2022-04-21 17:20:47 -0500

    Modulo - Formularios

---
+++ BlazorPeliculas/Client/Helpers/SelectorMultipleModel.cs
@@ -0,0 +1,13 @@
+﻿namespace BlazorPeliculas.Client.Helpers;
+
+public class SelectorMultipleModel
+{
+    public SelectorMultipleModel(string LLave, string Valor)
+    {
+        LLave = LLave;
+        Valor = Valor;
+    }
+
+    public string LLave { get; set; }
+    public string Valor { get; set; }
+}
---
+++ BlazorPeliculas/Shared/Entidades/GeneroPelicula.cs
@@ -0,0 +1,7 @@
+﻿namespace BlazorPeliculas.Shared.Entidades;
+
+public class GeneroPelicula
+{
+    public int PeliculaId { get; set; }
+    public int GeneroId { get; set; }
+}
