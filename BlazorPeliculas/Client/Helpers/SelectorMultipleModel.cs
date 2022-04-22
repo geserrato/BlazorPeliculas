6406ecfac272a346f59cbe5f403ecb3d50369794
Gerardo Estrella <gerardo.estrella@sygno.com.mx>
2022-04-21 22:30:15 -0500

    Cambio en llenado de generos seleccionado y no seleccionados

--- BlazorPeliculas/Client/Helpers/SelectorMultipleModel.cs
+++ BlazorPeliculas/Client/Helpers/SelectorMultipleModel.cs
@@ -2,11 +2,11 @@
 
 public class SelectorMultipleModel
 {
-    public SelectorMultipleModel(string LLave, string Valor)
-    {
-        LLave = LLave;
-        Valor = Valor;
-    }
+    //public SelectorMultipleModel(string LLave, string Valor)
+    //{
+    //    LLave = LLave;
+    //    Valor = Valor;
+    //}
 
     public string LLave { get; set; }
     public string Valor { get; set; }
