99d2e315f94bb24c5c61b58cb6fb3763d0d7bf22
Gerardo Estrella <gerardo.estrella@sygno.com.mx>
2022-05-20 10:05:56 -0500

    Proveedor Autenticacion JWT

---
+++ BlazorPeliculas/Client/Auth/ProveedorAutenticacionJWT.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Components.Authorization;
+
+namespace BlazorPeliculas.Client.Auth;
+
+public class ProveedorAutenticacionJWT : AuthenticationStateProvider
+{
+    public override Task<AuthenticationState> GetAuthenticationStateAsync()
+    {
+        throw new NotImplementedException();
+    }
+}
